4ed980342e7f47067cdfe968458e56a9f122f828
us4c0d3 <jwseok312@gmail.com>
2025-03-18 10:57:53 +0900

    chore: initial setting

---
+++ src/main/java/com/feelody/feelody_backend/FeelodyApplication.java
@@ -0,0 +1,13 @@
+package com.feelody.feelody_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeelodyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeelodyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feelody/feelody_backend/FeelodyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feelody.feelody_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeelodyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
