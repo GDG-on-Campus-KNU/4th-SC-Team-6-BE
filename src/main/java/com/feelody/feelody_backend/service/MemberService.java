515021cc994da5aeff3d934e5bad66d09b9977d6
us4c0d3 <jwseok312@gmail.com>
2025-04-28 17:07:25 +0900

    feat($Member): 불필요 패키지 삭제

--- src/main/java/com/feelody/feelody_backend/service/MemberService.java
+++ src/main/java/com/feelody/feelody_backend/service/MemberService.java
@@ -2,7 +2,6 @@ package com.feelody.feelody_backend.service;
 
 import com.feelody.feelody_backend.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
