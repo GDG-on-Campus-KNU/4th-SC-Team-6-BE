5fdb04a866556a3afb38be1a8e7a49acbb9bc230
us4c0d3 <jwseok312@gmail.com>
2025-04-28 16:50:38 +0900

    feat($Member): 누락된 생성자 추가

--- src/main/java/com/feelody/feelody_backend/entity/Member.java
+++ src/main/java/com/feelody/feelody_backend/entity/Member.java
@@ -4,16 +4,15 @@ import com.feelody.feelody_backend.global.common.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import java.util.UUID;
-import lombok.Getter;
 
 @Entity
-@Getter
 public class Member extends BaseEntity {
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String email;
 
     @Column(nullable = false, columnDefinition = "BINARY(16)")
     private UUID uuid;
 
+    protected Member() {}
 }
