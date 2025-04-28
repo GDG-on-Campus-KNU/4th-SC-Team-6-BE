8776d73bd20fa4690e05602852bcd413162707cf
us4c0d3 <jwseok312@gmail.com>
2025-04-28 17:07:02 +0900

    feat($Member): 불필요한 `@Autowired` 제거

--- src/main/java/com/feelody/feelody_backend/service/MemberService.java
+++ src/main/java/com/feelody/feelody_backend/service/MemberService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class MemberService {
-    @Autowired
+
     private final MemberRepository memberRepository;
 
 }
