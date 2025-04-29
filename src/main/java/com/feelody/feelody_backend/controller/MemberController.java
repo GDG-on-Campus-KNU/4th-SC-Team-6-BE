18788f8abbd60cc86428a97415bf468161eac697
Wooseok Jang <jwseok312@gmail.com>
2025-04-29 22:11:25 +0900

    Feature/issue#2: Member 도메인 생성 (#12)
    * feat(BaseEntity): 전체적으로 사용하기 위한 BaseEntity 정의
    
    - 필드 정의
      - id: Long, AutoIncremental
      - createdAt: LocalDateTime, update 불가
      - updatedAt: LocalDateTime, 수정된 시간 기록
    
    * feat($Member): member 엔티티 생성
    
    * feat($Member): member 연관 controller, service, repository 생성
    
    * feat($Member): 누락된 생성자 추가
    
    * feat($Member): 불필요한 `@Autowired` 제거
    
    * feat($Member): 불필요 패키지 삭제

---
+++ src/main/java/com/feelody/feelody_backend/controller/MemberController.java
@@ -0,0 +1,15 @@
+package com.feelody.feelody_backend.controller;
+
+import com.feelody.feelody_backend.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/member")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+}
