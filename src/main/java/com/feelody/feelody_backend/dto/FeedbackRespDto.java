e3171f7f8b6b35b1c261a583639531a8126ef662
dongsu_ko <127917642+kdongsu5509@users.noreply.github.com>
2025-05-08 20:25:44 +0900

    fix(#issue17)/ AllArgsConstructor 제거
    https://github.com/GDG-on-Campus-KNU/4th-SC-Team-6-BE/pull/21에서 받은 피드백 반영

--- src/main/java/com/feelody/feelody_backend/dto/FeedbackRespDto.java
+++ src/main/java/com/feelody/feelody_backend/dto/FeedbackRespDto.java
@@ -1,19 +1,12 @@
 package com.feelody.feelody_backend.dto;
 
 import java.time.LocalDate;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Getter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class FeedbackRespDto {
-    private Long id;
-    private String title;
-    private String artist;
-    private int score;
-    private LocalDate createdAt; // 연습일자
+public record FeedbackRespDto(
+        Long id,
+        String title,
+        String artist,
+        int score,
+        LocalDate createdAt // 연습일자
+) {
 }
