1c0503bdecffc78b777bc82ad8b3987dd7e0a685
us4c0d3 <us4c0d3@gmail.com>
2025-05-13 03:23:29 +0900

    feat: key json을 사용하지 않도록 변경

--- src/main/java/com/feelody/feelody_backend/global/config/GcsConfig.java
+++ src/main/java/com/feelody/feelody_backend/global/config/GcsConfig.java
@@ -5,6 +5,7 @@ import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,17 +18,26 @@ public class GcsConfig {
 
     @Value("${spring.cloud.gcp.project-id}")
     private String projectId;
-    
+
     @Bean
     public Storage storage() throws IOException {
-        try (var inputStream = new FileInputStream(
-            gcpCredentialsLocation.replaceFirst("file:", ""))) {
-            GoogleCredentials credentials = GoogleCredentials.fromStream(inputStream);
-            return StorageOptions.newBuilder()
-                .setProjectId(projectId)
-                .setCredentials(credentials)
-                .build()
-                .getService();
+        GoogleCredentials credentials;
+
+        if (gcpCredentialsLocation == null || gcpCredentialsLocation.isBlank()
+            || gcpCredentialsLocation.equals("disabled")) {
+            credentials = GoogleCredentials.getApplicationDefault();
+        } else {
+            try (InputStream inputStream = new FileInputStream(
+                gcpCredentialsLocation.replaceFirst("file:", ""))) {
+                credentials = GoogleCredentials.fromStream(inputStream);
+            }
         }
+
+        return StorageOptions.newBuilder()
+            .setProjectId(projectId)
+            .setCredentials(credentials)
+            .build()
+            .getService();
+
     }
 }
