d32cef4ccc9a3b3b34d113bfcbc25b129e816ac4
us4c0d3 <jwseok312@gmail.com>
2025-05-16 21:34:17 +0900

    revert

--- src/main/java/com/feelody/feelody_backend/global/common/GcsFileManager.java
+++ src/main/java/com/feelody/feelody_backend/global/common/GcsFileManager.java
@@ -8,12 +8,10 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
-@Profile("!test")
 @Component
 @RequiredArgsConstructor
 public class GcsFileManager {
